20d23553cc9255f9c8aa82c95b4c86cf83ffc20d
mozhnopersik <mozhnopersik@mail.ru>
2023-10-10 11:18:58 +0300

    Initial Commit

---
+++ YourUniverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourUniverse
+//
+//  Created by Вероника Карпова on 10.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
