758a04115d57206cb3d7a65ee5bae1bf02cdb3e5
mozhnopersik <mozhnopersik@mail.ru>
2023-10-10 14:39:23 +0300

    All changes

---
+++ YourUniverse/Models/APODResponse.swift
@@ -0,0 +1,8 @@
+//
+//  APODResponse.swift
+//  YourUniverse
+//
+//  Created by Вероника Карпова on 10.10.2023.
+//
+
+import Foundation
---
+++ YourUniverse/Services/RequestManager.swift
@@ -0,0 +1,8 @@
+//
+//  RequestManager.swift
+//  YourUniverse
+//
+//  Created by Вероника Карпова on 10.10.2023.
+//
+
+import Foundation
